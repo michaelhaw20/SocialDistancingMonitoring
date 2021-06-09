495666f7a34c5ac0a7af3787be9d07959c852714
DavidApri19 <43982366+davidapri19@users.noreply.github.com>
2021-06-09 17:07:23 +0700

    Update DetailActivity.kt

--- app/src/main/java/com/example/socialdistancing/DetailActivity.kt
+++ app/src/main/java/com/example/socialdistancing/DetailActivity.kt
@@ -1,33 +1,50 @@
 package com.example.socialdistancing
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import androidx.annotation.StringRes
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
-import com.example.socialdistancing.databinding.ActivityDetailBinding
-import com.google.android.material.tabs.TabLayoutMediator
-import com.loopj.android.http.AsyncHttpClient
-import com.loopj.android.http.AsyncHttpResponseHandler
-import cz.msebera.android.httpclient.Header
-import org.json.JSONObject
+import com.alphabangkit.socialdistancing.databinding.ActivityDetailBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 class DetailActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityDetailBinding
+    private lateinit var detailbinding: ActivityDetailBinding
     companion object {
         const val EXTRA_USER = "extra_user"
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityDetailBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        supportActionBar?.title="Detail Pelanggaran"
+        detailbinding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(detailbinding.root)
+        supportActionBar?.title = "Detail Pelanggaran"
         val urlphoto = intent.getStringExtra(EXTRA_USER)
-
         Glide.with(applicationContext)
-                .load(urlphoto) //menyesuaikan  photo, idku, ii
-                .apply(RequestOptions())
-                .into(binding.imgDetail)
+            .load(urlphoto)
+            .apply(RequestOptions())
+            .into(detailbinding.imgDetail)
+        fun Date.toString(format: String, locale: Locale = Locale.getDefault()): String {
+            val formatter = SimpleDateFormat(format, locale)
+            return formatter.format(this)
+        }
+        fun getCurrentDateTime() : Date {
+            return Calendar.getInstance().time
+        }
+        val date = getCurrentDateTime()
+        val dateInString = date.toString("yyyy/MM/dd")
+        detailbinding.tanggalDetail.text = dateInString
+        detailbinding.btnNotif.setOnClickListener {
+            /*
+            val intentnot = Intent(this@DetailActivity, NotifActivity::class.java)
+            startActivity(intentnot)*/
+
+            val moveWithObjectIntent = Intent(this@DetailActivity, NotifActivity::class.java)
+                moveWithObjectIntent.putExtra(NotifActivity.EXTRA_IMAGE, urlphoto)
+                Log.d("mbuh", urlphoto.toString())
+                startActivity(moveWithObjectIntent)
+
+        }
     }
 }
